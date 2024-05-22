60cf989c8d9fa6560cd99617057490c8cb62a495
Nathan Felber <nathan@pascaline.org>
2024-05-22 01:00:54 +0000

    Improve sequencer cli output

--- src/sequencer.py
+++ src/sequencer.py
@@ -6,87 +6,121 @@ from fprime_gds.common.data_types.event_data import EventData
 from fprime_gds.common.testing_fw.api import IntegrationTestAPI
 from src.parser.parser import ExpectEventInstruction, ExpectTelemetryInstruction, Sequence
 
+def make_green(s: str) -> str:
+    return f"\033[92m{s}\033[0m"
 
-def run_sequence(seq: Sequence, api: IntegrationTestAPI):
-    starting_time_s = time.time()
-    def elapsed_time_s():
-        return time.time() - starting_time_s
+def make_red(s: str) -> str:
+    return f"\033[91m{s}\033[0m"
 
-    commands = seq.get_ordered_commands()
-    max_send_time_digits = len(str(commands[-1].send_time_ms))
-
-    for command in commands:
-        # Sleep until next command
-        time.sleep(max(0.001 * command.send_time_ms - elapsed_time_s(), 0))
-        # Send command
-        print(f"[{round(1000 * elapsed_time_s()):{max_send_time_digits}} ms]: Sending command {command.command} {' '.join(command.args)}")
-        api.send_command(command.command, command.args)
+class Sequencer:
+    def __init__(self, api: IntegrationTestAPI) -> None:
+        self.api = api
 
 
-def find_event(expected_event: ExpectEventInstruction, events: list[EventData], starting_time: float) -> EventData | None:
-    for event in events:
-        match_ = True
-        match_ &= 0.001 * expected_event.start_time_ms <= event.get_time().get_float() - starting_time <= 0.001 * expected_event.end_time_ms
-        match_ &= expected_event.event == event.get_severity() or expected_event.event == event.template.get_full_name()
-        if expected_event.expected_value != None:
-            if expected_event.is_regex:
-                match_ &= re.search(expected_event.expected_value, event.get_display_text()) != None
+    def run_and_validate_sequence(self, seq: Sequence) -> bool:
+        header = f" [RUNNING TEST {seq.name}] "
+        print(f"{header:=^80s}")
+
+        starting_time = time.time()
+        self.run_sequence(seq)
+
+        remaining_time = max(0, starting_time + 0.001 * seq.get_duration() - time.time())
+        print(f"\nWaiting {remaining_time:.2f} seconds for the sequence to finish...")
+        time.sleep(remaining_time)
+        print()
+
+        success = self.validate_sequence(seq, starting_time)
+
+        footer = f" [TEST {seq.name} {make_green('PASSED') if success else make_red('FAILED')}] "
+        print(f"\n{footer:=^89s}")
+
+        return success
+
+
+    def run_sequence(self, seq: Sequence):
+        starting_time_s = time.time()
+        def elapsed_time_s():
+            return time.time() - starting_time_s
+
+        commands = seq.get_ordered_commands()
+        max_send_time_digits = len(str(commands[-1].send_time_ms))
+
+        for command in commands:
+            # Sleep until next command
+            time.sleep(max(0.001 * command.send_time_ms - elapsed_time_s(), 0))
+            # Send command
+            print(f"[{round(1000 * elapsed_time_s()):{max_send_time_digits}} ms]: Sending command {command.command} {' '.join(command.args)}")
+            self.api.send_command(command.command, command.args)
+
+
+    def find_matching_event(self, event: ExpectEventInstruction, starting_time: float) -> EventData | None:
+        for received_event in self.api.get_event_test_history().retrieve():
+            match_ = True
+            match_ &= 0.001 * event.start_time_ms <= received_event.get_time().get_float() - starting_time <= 0.001 * event.end_time_ms
+            match_ &= event.event == received_event.get_severity() or event.event == received_event.template.get_full_name()
+            if event.expected_value != None:
+                if event.is_regex:
+                    match_ &= re.search(event.expected_value, received_event.get_display_text()) != None
+                else:
+                    match_ &= event.expected_value == received_event.get_display_text()
+            if match_:
+                return received_event
+        return None
+
+    def find_matching_telemetry(self, telemetry: ExpectTelemetryInstruction, starting_time: float) -> ChData | None:
+        for received_telemetry in self.api.get_telemetry_test_history().retrieve():
+            match_ = True
+            match_ &= 0.001 * telemetry.start_time_ms <= received_telemetry.get_time().get_float() - starting_time <= 0.001 * telemetry.end_time_ms
+            match_ &= telemetry.channel == received_telemetry.template.get_full_name()
+            if telemetry.expected_value != None:
+                if telemetry.is_regex:
+                    match_ &= re.search(telemetry.expected_value, str(received_telemetry.get_display_text())) != None
+                else:
+                    match_ &= telemetry.expected_value == received_telemetry.get_display_text()
+            if match_:
+                return received_telemetry
+        return None
+
+
+    def validate_sequence(self, seq: Sequence, starting_time: float) -> bool:
+        success = True
+
+        print(f"{' [VALIDATING EVENTS] ':-^80s}")
+
+        for expected_event in seq.event_instrs:
+            matching_event = self.find_matching_event(expected_event, starting_time)
+            success &= (matching_event != None) == expected_event.is_expected
+            if matching_event != None:
+                timing = f"[{round(1000 * (matching_event.get_time().get_float() - starting_time))} ms]"
+                severity = matching_event.get_severity()
+                name = matching_event.template.get_full_name()
+                value = "" if matching_event.get_display_text() == "" else f"\"{matching_event.get_display_text()}\""
+                match_ = f"{timing} {severity} {name} {value}"
             else:
-                match_ &= expected_event.expected_value == event.get_display_text()
-        if match_:
-            return event
-    return None
+                match_ = "None"
+            timing = f"[{expected_event.start_time_ms}:{expected_event.end_time_ms}]"
+            event = f"EXPECT{'' if expected_event.is_expected else ' NO'} EVENT {expected_event.event}"
+            value = "" if expected_event.expected_value == None else f" {'re' if expected_event.is_regex else ''}\"{expected_event.expected_value}\""
+            result = f"{make_green('[OK]') if (matching_event != None) == expected_event.is_expected else make_red('[FAIL]')}"
+            print(f"{timing} {event}{value}: {result} ~> {match_}")
 
-def find_telemetry(expected_telemetry: ExpectTelemetryInstruction, tel_readings: list[ChData], starting_time: float) -> ChData | None:
-    for reading in tel_readings:
-        match_ = True
-        match_ &= 0.001 * expected_telemetry.start_time_ms <= reading.get_time().get_float() - starting_time <= 0.001 * expected_telemetry.end_time_ms
-        match_ &= expected_telemetry.channel == reading.template.get_full_name()
-        if expected_telemetry.expected_value != None:
-            if expected_telemetry.is_regex:
-                match_ &= re.search(expected_telemetry.expected_value, str(reading.get_display_text())) != None
+        print()
+        print(f"{' [VALIDATING TELEMETRY] ':-^80s}")
+
+        for expected_telemetry in seq.telemetry_instrs:
+            matching_telemetry = self.find_matching_telemetry(expected_telemetry, starting_time)
+            success &= (matching_telemetry != None) == expected_telemetry.is_expected
+            if matching_telemetry != None:
+                timing = f"[{round(1000 * (matching_telemetry.get_time().get_float() - starting_time))} ms]"
+                name = matching_telemetry.template.get_full_name()
+                value = "" if matching_telemetry.get_display_text() == "" else f"\"{matching_telemetry.get_display_text()}\""
+                match_ = f"{timing} {name} {value}"
             else:
-                match_ &= expected_telemetry.expected_value == reading.get_display_text()
-        if match_:
-            return reading
-    return None
+                match_ = "None"
+            timing = f"[{expected_telemetry.start_time_ms}:{expected_telemetry.end_time_ms}]"
+            event = f"EXPECT{'' if expected_telemetry.is_expected else ' NO'} TELEMETRY {expected_telemetry.channel}"
+            value = "" if expected_telemetry.expected_value == None else f" {'re' if expected_telemetry.is_regex else ''}\"{expected_telemetry.expected_value}\""
+            result = f"{make_green('[OK]') if (matching_telemetry  != None) == expected_telemetry.is_expected else make_red('[FAIL]')}"
+            print(f"{timing} {event}{value}: {result} ~> {match_}")
 
-
-def validate_sequence(seq: Sequence, starting_time: float, api: IntegrationTestAPI):
-    ok = "\033[92m[OK]\033[0m"
-    fail = "\033[91m[FAIL]\033[0m"
-
-    print("[VALIDATING EVENTS]")
-
-    for expected_event in seq.event_instrs:
-        matching_event = find_event(expected_event, api.get_event_test_history().retrieve(), starting_time)
-        if matching_event != None:
-            timing = f"[{round(1000 * (matching_event.get_time().get_float() - starting_time))} ms]"
-            severity = matching_event.get_severity()
-            name = matching_event.template.get_full_name()
-            value = "" if matching_event.get_display_text() == "" else f"\"{matching_event.get_display_text()}\""
-            match_ = f"{timing} {severity} {name} {value}"
-        else:
-            match_ = "None"
-        timing = f"[{expected_event.start_time_ms}:{expected_event.end_time_ms}]"
-        event = f"EXPECT{'' if expected_event.is_expected else ' NO'} EVENT {expected_event.event}"
-        value = "" if expected_event.expected_value == None else f" {'re' if expected_event.is_regex else ''}\"{expected_event.expected_value}\""
-        result = f"{ok if (matching_event != None) == expected_event.is_expected else fail}"
-        print(f"{timing} {event}{value}: {result} ~> {match_}")
-
-    print("[VALIDATING TELEMETRY]")
-
-    for expected_telemetry in seq.telemetry_instrs:
-        matching_telemetry = find_telemetry(expected_telemetry, api.get_telemetry_test_history().retrieve(), starting_time)
-        if matching_telemetry != None:
-            timing = f"[{round(1000 * (matching_telemetry.get_time().get_float() - starting_time))} ms]"
-            name = matching_telemetry.template.get_full_name()
-            value = "" if matching_telemetry.get_display_text() == "" else f"\"{matching_telemetry.get_display_text()}\""
-            match_ = f"{timing} {name} {value}"
-        else:
-            match_ = "None"
-        timing = f"[{expected_telemetry.start_time_ms}:{expected_telemetry.end_time_ms}]"
-        event = f"EXPECT{'' if expected_telemetry.is_expected else ' NO'} TELEMETRY {expected_telemetry.channel}"
-        value = "" if expected_telemetry.expected_value == None else f" {'re' if expected_telemetry.is_regex else ''}\"{expected_telemetry.expected_value}\""
-        result = f"{ok if (matching_telemetry != None) == expected_telemetry.is_expected else fail}"
-        print(f"{timing} {event}{value}: {result} ~> {match_}")
+        return success
